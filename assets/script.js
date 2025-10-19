5a5aed2830d58c6aaceabe50387909119fde2de5
Guilherme <gui.morais.andrade@gmail.com>
2025-10-19 16:14:04 -0300

    Realizando alggumas alterações básicas nos arquivos html, css e js

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ function efeitoEscrever(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = ' ';
     textoArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 150 * i);
+        setTimeout(() => elemento.innerHTML += letra, 170 * i);
     });
 }
 
